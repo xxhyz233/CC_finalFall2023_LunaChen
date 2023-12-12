1bbeb5095027cb197bcd3b60c02515b3f0e3639d
xxhyz233 <49100459+xxhyz233@users.noreply.github.com>
2023-12-11 22:42:17 -0500

    Purely Audio Reactive Version
    No timestamps, just audio reaction.

--- sketch.js
+++ sketch.js
@@ -61,81 +61,7 @@ function draw()
 
     milliSec = millis();
     spectrum = fft.analyze(bandWidth);
-    // Setting cutIndex from timestamps
-    if (milliSec > 0 && milliSec < cut2)
-    {
-        cutIndex = 1;
-    }
-    else if (milliSec > cut2 && milliSec < cut3)
-    {
-        cutIndex = 2;
-    }
-    else if (milliSec > cut3 && milliSec < cut4)
-    {
-        cutIndex = 3;
-        overlayIndex = 1;
-    }
-    else if (milliSec > cut4 && milliSec < cut5)
-    {
-        cutIndex = 4;
-    }
-    else if (milliSec > cut5 && milliSec < cut6)
-    {
-        cutIndex = 5;
-    }
 
-    if (overlayIndex === 1)
-    {
-
-    }
-
-    // Things to be generated at different cuts
-    if (cutIndex === 1)
-    {
-        amp = fft.getEnergy('highMid');
-        let flicker = map(amp, 0, 255, 20, 60);
-        background(flicker);
-    }
-    else if (cutIndex === 2)
-    {   
-        push();
-        noStroke();
-        fill(255);
-        textSize(20);
-        text('I really cant do it.', width/2, height/2);
-        let interval = milliSec - cut2;
-        
-        scaleValue = map(interval, 0, 10000, 1, 0.1);
-        scale(scaleValue);
-        pop();
-    }
-
-    else if (cutIndex === 3)
-    {
-        amp = fft.getEnergy('highMid');
-        let flicker = map(amp, 0, 255, 20, 60);
-        background(flicker);
-
-        auSpectrum(spectrum, width/2, height/2, 20, blue, 10);
-        // getEnergy returns the 'energy' of the song from the lowMid frequencies of the music
-        // Returns a value from 0-255
-        amp = fft.getEnergy('lowMid');
-        // Once the music reaches a certain peek,
-        if(amp > 250)
-        {
-            maxFontSize = floor(random(200,500));
-            
-            // Adjusting the remainder values makes the text populating more frequent
-            if(floor(milliSec) % 5 === 0)
-            {
-                populateText('I really cant do it.');
-            }
-            // Spectrum in the center
-        }
-    }
-
-    else if(cutIndex === 4)
-    {
         // White layer on bottom
         auSpectrum(spectrum, width/2, height/2, 500, color(255,255,255, 100), 45);
         // Blue layer on top
@@ -169,14 +95,7 @@ function draw()
         {
             isFeedback = false;
         }
-    }
-
-    else if (cutIndex === 5)
-    {
-        amp = fft.getEnergy('highMid');
-        let flicker = map(amp, 0, 255, 20, 60);
-        background(flicker);
-    }
+    
 }
 
 function keyReleased()
